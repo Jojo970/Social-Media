c69b1655d774b83fcea5b1f5c59e807274f38caf
Jong Lee <jjleejong@gmail.com>
2023-05-24 16:23:17 -0500

    added front end

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js"
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
+
 
 
 // Configs
@@ -65,5 +69,8 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }). then( () => {
-    app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
+    app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 }).catch ( (error) => console.log( `${error} did not connect`))
